43e2fc267553be59a1860ad830913d060e11d2d3
jiradawi <cutiemoon08@yahoo.com>
2026-07-12 00:09:43 +0700

    add icon

--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -1,10 +1,11 @@
+import { AiOutlinePlus } from "react-icons/ai";
 
 
 const AddTask = () => {
     return (
     <div>   
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Add New Task
+        <button className="flex items-center gap-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Add New Task <AiOutlinePlus size={20} className="ml-2" />
         </button>
     </div>
     
