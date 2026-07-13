9b9dc7027046858a524e36181082a16c40626450
jiradawi <cutiemoon08@yahoo.com>
2026-07-13 23:42:15 +0700

    create table for tasks

--- app/components/TodoList.tsx
+++ app/components/TodoList.tsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const TodoList = () => {
-    return <div>Todo List</div>
+  return (
+    <table className="border-collapse border border-gray-400">
+      <thead>
+        <tr>
+          <th className="border border-gray-300">Task</th>
+          <th className="border border-gray-300">Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td className="border border-gray-300">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+          <td className="border border-gray-300">Malcolm Lockyer</td>
+        </tr>
+      </tbody>
+    </table>
+  );
 };
 
 export default TodoList;
